eb5c36f70dddee24978637183344ac4777513b38
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2022-02-10 17:07:11 -0300

    ID_45

---
+++ ID_45_triangular_pentagonal_hexagonal/ID_45_julia.jl
@@ -0,0 +1,48 @@
+# function triangular(n::Int64)::Int64
+#     return (n * (n + 1)) / 2
+# end
+
+
+# function isTriangular(T::Int64)::Bool
+#     return isinteger((-1 + √(1 + 8 * T)) / 2) ? true : false
+# end
+
+
+# function pentagonal(n::Int64)::Int64
+#     return n * (3 * n - 1) / 2
+# end
+
+
+function isPentagonal(P::Int64)::Bool
+    return isinteger((1 + √(1 + 24 * P)) / 6) ? true : false
+end
+
+
+function hexagonal(n::Int64)::Int64
+    return n * (2 * n - 1)
+end
+
+
+# function isHexagonal(H::Int64)::Bool
+#     return isinteger((1 + √(1 + 8 * H)) / 4) ? true : false
+# end
+
+
+function nextHexagonal(H::Int64)::Int64
+    return hexagonal(Int((1 + √(1 + 8 * H)) / 4) + 1)
+end
+
+
+function findNextTPH(n::Int64)::Int64
+    num = nextHexagonal(n)
+    while !isPentagonal(num) # all hexagonal are triangular, so generate H and check if is P
+        num = nextHexagonal(num)
+    end
+    return num
+end
+
+
+const NUM = 40755
+println(@time findNextTPH(NUM))
+
+# answer: 1533776805
---
+++ ID_45_triangular_pentagonal_hexagonal/ID_45_julia_v2.jl
@@ -0,0 +1,24 @@
+isPentagonal(P::Int64)::Bool = isinteger((1 + √(1 + 24 * P)) / 6) ? true : false
+
+
+hexagonal(n::Int64)::Int64 = n * (2 * n - 1)
+
+
+hIndex(num::Int64)::Int64 = Int((1 + √(1 + 8 * num)) / 4)
+
+
+function findNextTPH(n::Int64)::Int64
+    index = hIndex(n) + 1
+    num = hexagonal(index)
+    while !isPentagonal(num) # all hexagonal are triangular, so generate H and check if is P
+        index += 1
+        num = hexagonal(index)
+    end
+    return num
+end
+
+
+const NUM = 40755
+println(@time findNextTPH(NUM))
+
+# answer: 1533776805
